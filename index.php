5a8e1a4df99d643eabc60d346da9a839ad287efa
CarolinaOkamoto <84644754+CarolinaOkamoto@users.noreply.github.com>
2021-05-22 20:05:00 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="author" content="Carolina Okamoto">
+    <title>Leitor de XML</title>
+    <link rel="icon" href="imagens/favicon.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+</head>
+<body>
+    <?php
+    require_once('inc/topo.php');
+    ?>
+    <div class="wrapper">
+        <div class="container conteudoPage">
+            <?php
+              require_once('inc/home.php');
+            ?>
+        </div>
+    </div>
+    <?php
+        require_once('inc/rodape.php');
+    ?>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="  crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"  integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30="  crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+</body>
+</html>
